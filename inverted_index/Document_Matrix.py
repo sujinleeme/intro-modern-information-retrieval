a67e81baf0a78c5d7ef90f4559dfb97ffc79b43d
sujinleeme <sujinlee.me@gmail.com>
2018-04-09 16:44:49 +0900

    style: Change classname to Document_Matrix and delete print()

--- inverted_index/Document_Matrix.py
+++ inverted_index/Document_Matrix.py
@@ -3,7 +3,6 @@
 
 # A term-document incidence matrix.
 import re
-from collections import OrderedDict
 from utils import sortByKeys
 
 class Document():
@@ -14,23 +13,22 @@ class Document():
     content = open(filename, "r").read().lower().split()
     self._wordlist.extend(content)
 
-class Boolean_Retrieval():
+class Document_Matrix():
   def __init__(self):
     self._docs = {}
     self._dictionary = {} # a dictionary of terms
-    self._incidence_matrix = {} # term-document sincidence matrix for document collection.
+    self._incidence_matrix = {} # term-document incidence matrix for document collection.
     self._inverted_index = {} # inverted index representation for collection
 
   def add_documents(self, doc):
     if doc not in self._docs:
-      self._docs[doc] =  Document()
+      self._docs[doc] = Document()
       self._docs[doc].tokenize(doc)
 
   # building an index by sorting adnd grouping 
   def get_word_group_list(self):
     word = [value._wordlist for key, value in self._docs.items()]
     word = set(sum(word, []))
-    #print(word)
     self._dictionary = sorted(word, reverse=False)
 
   # make dictionary format to set document Id to store word counting results
@@ -38,7 +36,6 @@ class Boolean_Retrieval():
     init_doc_id = dict([(re.findall(r'[ \w-]+?(?=\.)', key)[0]
                       , 0) for key in self._docs.keys()])
     dictionary = dict([(key, init_doc_id) for key in self._dictionary])
-    print(dictionary)
     self._incidence_matrix = sortByKeys(dictionary)
   
   def search_word(self, filename):
